7aca6d483e8249adedb8e1fad6d2bc2e7143fe6f
Christopher Pezza <chris.pezza@hey.com>
2020-10-17 11:45:05 -0400

    Add JS

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,3 +1,11 @@
+import 'bridgetown-quick-search';
+import {Application} from 'stimulus';
+import {definitionsFromContext} from 'stimulus/webpack-helpers';
+import TabsController from 'stimulus-tabs';
+
 import "../styles/index.scss"
 
-console.info("Bridgetown is loaded!")
+const application = Application.start();
+application.register('tabular', TabsController);
+const context = require.context('./controllers', true, /\.js$/);
+application.load(definitionsFromContext(context));
